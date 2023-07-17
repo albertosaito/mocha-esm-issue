08a1c6ce00ac6e2755ac62d3037b6fd5134ec4fe
Nobuji Saito <14101642+albertosaito@users.noreply.github.com>
2023-07-17 11:42:17 -0400

    Import JSON tricks

--- src/code.js
+++ src/code.js
@@ -2,6 +2,20 @@
 
 import * as fs from 'fs';
 
+// import JSON (1): Using regular import/assert syntax (NOTE THIS IS STILL EXPERIMENTAL)
+import * as p from '../package.json' assert  { type: 'json' };
+// import JSON (2)
+const p2 = await import('../package.json', { assert: { type: 'json' } });
+// "import & parse" JSON (3)
+import { readFile } from 'fs/promises';
+const p3 = JSON.parse(await readFile(new URL('../package.json', import.meta.url)));
+// createRequire JSON (4)
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const p4 = require("../package.json");
+
+console.log(p.default.name, '===', p2.default.name, '===', p3.name, '===', p4.name);
+
 export default function myFunction() {
   // Use optional chaining
   if (fs?.chmodSync) {
